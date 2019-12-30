5fe6a383a14c52fb8489aa1856e3a8b93c4496de
Louwrens Labuschagne <louwjlabuschagne@gmail.com>
2019-12-30 14:26:23 +0200

    Getting going with whatsapp
    But something is going wrong with sending a message to myself

--- notebooks/autherise.py
+++ notebooks/autherise.py
@@ -5,7 +5,7 @@ import spotipy.util as util
 import configparser
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('../config.ini')
 
 scopes = ['ugc-image-upload',
           'user-read-playback-state',
