c4fefa7f5d3ce2e8fddf6f8984e29c635802757f
Louwrens Labuschagne <louwjlabuschagne@gmail.com>
2020-01-05 08:52:24 +0200

    Can read and write from google calendar,
    Now just need to incorporate it into the app

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,6 +1,18 @@
 MIDDELKLAS_YT_ID = 'BzGOPUwr4-Q'
 KOPSTAMP_YT_ID = 's4LEe7K96-U'
 
+SUPPORTED_PLATFORMS = ['SPOTIFY',
+                       'INSTAGRAM',
+                       'FACEBBOOK',
+                       'YOUTUBE']
+SUPPORTED_SONGS = ['RAAK_N_BIETJIE_FUCKED',
+                   'MY_DOLLA_NEE',
+                   'SEEMAN',
+                   'KOPSTAMP',
+                   '24',
+                   'MIDDELKLAS_MIDDELMAN',
+                   'SKIET_REENBOE_OP_FASCISME']
+
 SUPPORTED_RAW_LOG_PLATFORMS = ['SPOTIFY',
                                'INSTAGRAM',
                                'FACEBBOOK',
